66455e7375bbd105a84fd1a06c62f1f030fed68b
limed <limed@mozilla.com>
2019-02-01 12:40:55 -0800

    Fixing some typos

--- info/main.tf
+++ info/main.tf
@@ -4,12 +4,12 @@ locals {
   state_bucket = "${var.state_bucket == "" ? "deploy-state-${data.aws_caller_identity.current.account_id}" : var.state_bucket}"
 }
 
-data "terrafrom_remote_state" "info" {
+data terraform_remote_state "info" {
   backend = "${var.backend}"
 
   config = {
     bucket = "${local.state_bucket}"
-    name   = "${var.state_file}"
+    key    = "${var.state_file}"
     region = "${var.region}"
   }
 }
--- info/outputs.tf
+++ info/outputs.tf
@@ -1,3 +1,11 @@
+output "account_id" {
+  value = "${data.aws_caller_identity.current.account_id}"
+}
+
+output "state_bucket" {
+  value = "${local.state_bucket}"
+}
+
 output "vpc_id" {
   value = "${data.terraform_remote_state.info.vpc_id}"
 }
